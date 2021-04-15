7115aed172aecbf30511c0d65146ba4a0cd562fc
ofirlevy6658 <ofirlevy6658@gmail.com>
2021-04-15 16:50:26 +0300

    added functions

--- app.js
+++ app.js
@@ -1,4 +1,77 @@
+const {
+	addUser,
+	deposit,
+	updateCredit,
+	withdraw,
+	validationMoney,
+	transferMoney,
+} = require("./utils");
+
 const express = require("express");
 const app = express();
 
 app.use(express.json());
+
+// app.get("/api/user", (req, res) => {
+// 	try {
+// 		res.status(200).json(getMovies());
+// 	} catch (e) {
+// 		res.status(400).send({ error: e.message });
+// 	}
+// });
+
+app.post("/api/user", (req, res) => {
+	console.log(req.body);
+	try {
+		addUser({ cash: 515, dsadas: 3, dasdas: 1 });
+		res.status(201).send("User added");
+	} catch (e) {
+		res.status(400).send({ error: e.message });
+	}
+});
+
+app.put("/api/deposit/:id", (req, res) => {
+	try {
+		validationMoney(req.body);
+		deposit(req.params.id, req.body);
+		res.status(201).send("user cash update");
+	} catch (e) {
+		res.status(400).send({ error: e.message });
+	}
+});
+
+app.put("/api/credit/:id", (req, res) => {
+	try {
+		validationMoney(req.body);
+		updateCredit(req.params.id, req.body);
+		res.status(201).send("user credit update");
+	} catch (e) {
+		res.status(400).send({ error: e.message });
+	}
+});
+
+app.put("/api/withdraw/:id", (req, res) => {
+	try {
+		validationMoney(req.body);
+		withdraw(req.params.id, req.body);
+		res.status(201).send("withdraw succeed");
+	} catch (e) {
+		res.status(400).send({ error: e.message });
+	}
+});
+
+app.put("/api/transferMoney/:id", (req, res) => {
+	console.log("test");
+	try {
+		validationMoney(req.body);
+		transferMoney(req.params.id, req.body);
+		res.status(201).send("transfer money succeed");
+	} catch (e) {
+		res.status(400).send({ error: e.message });
+	}
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+	console.log("listening on port 3000");
+});
---
+++ utils.js
@@ -0,0 +1,92 @@
+const { v4: uuidv4 } = require("uuid"); // generate uniqe id
+const fs = require("fs");
+const { send } = require("process");
+
+function addUser({ cash = 0, credit = 0 }) {
+	// deafult value of 0 if not provided
+	if (cash > 999999 || cash < 0 || credit > 5000 || credit < 0)
+		throw new Error("bad parameters");
+	const user = { cash, credit, id: uuidv4() };
+	const users = loadUsers();
+	users.push(user);
+	saveUser(users);
+}
+
+function deposit(id, { amount }) {
+	const users = loadUsers();
+	const user = users.find((el) => el.id === id);
+	if (!user) throw new Error("User not found");
+	const newCash = parseInt(user.cash) + parseInt(amount);
+	user.cash = newCash;
+	saveUser(users);
+}
+
+function updateCredit(id, { credit }) {
+	const users = loadUsers();
+	const user = users.find((el) => el.id === id);
+	if (!user) throw new Error("User not found");
+	const newCredit = parseInt(user.credit) + parseInt(credit);
+	user.credit = newCredit;
+	saveUser(users);
+}
+
+function withdraw(id, { amount }) {
+	const users = loadUsers();
+	const user = users.find((el) => el.id === id);
+	if (!user) throw new Error("User not found");
+	const newCash = parseInt(user.cash) - parseInt(amount);
+	const currentCash = parseInt(user.cash) + parseInt(user.credit);
+	if (amount > currentCash)
+		throw new Error(`withdraw rejected, your balance is ${currentCash}$`);
+	user.cash = newCash;
+	saveUser(users);
+}
+
+function transferMoney(senderID, { amount, id }) {
+	const users = loadUsers();
+	const reciver = users.find((el) => el.id === id);
+	const sender = users.find((el) => el.id === senderID);
+	if (!reciver || !sender) throw new Error("User not found");
+	if (reciver === sender) throw new Error("sender and reciver are the same");
+	if (!isPossibleWithdraw(sender, amount))
+		throw new Error(`transfer money rejected not enough money in the account`);
+	sender.cash = parseInt(sender.cash) - parseInt(amount);
+	reciver.cash = parseInt(reciver.cash) + parseInt(amount);
+	saveUser(users);
+}
+
+function isPossibleWithdraw(user, amount) {
+	const userCurrentMoney = parseInt(user.cash) + parseInt(user.credit);
+	if (userCurrentMoney >= parseInt(amount)) {
+		return true;
+	}
+	return false;
+}
+
+function validationMoney({ amount }) {
+	if (!amount) throw new Error("parameter not provided");
+	if (amount < 1) throw new Error("parameter must be positive");
+}
+
+function saveUser(user) {
+	fs.writeFileSync("./database/users.json", JSON.stringify(user));
+}
+
+function loadUsers() {
+	try {
+		const dataBuffer = fs.readFileSync("./database/users.json");
+		const dataJSON = dataBuffer.toString();
+		return JSON.parse(dataJSON);
+	} catch (e) {
+		return [];
+	}
+}
+
+module.exports = {
+	addUser,
+	deposit,
+	updateCredit,
+	withdraw,
+	validationMoney,
+	transferMoney,
+};
