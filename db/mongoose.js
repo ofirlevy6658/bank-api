e072482db5be74ba1e4ac212134f2df757388667
ofirlevy6658 <ofirlevy6658@gmail.com>
2021-04-24 19:52:00 +0300

    fix: refactor hard coded connection string to Heroku config var

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-	"mongodb+srv://dbBank:6105935z@cluster0.u2wgr.mongodb.net/bank-manager-api?retryWrites=true&w=majority",
-	{
-		useNewUrlParser: true,
-		useCreateIndex: true,
-	}
-);
+const uri = process.env.MONGODB_URI;
+console.log(uri);
+mongoose.connect("uri", {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+});
+
+// heroku config:set MONGODB_URI=mongodb+srv://dbBank:6105935z@cluster0.u2wgr.mongodb.net/bank-manager-api?retryWrites=true&w=majority
